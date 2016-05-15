19e0374100ebe8385bd7404ab3dd6d5bc7eae737
MaidenBeast <fel.sallustio@gmail.com>
2016-05-15 17:18:54 +0200

    Refactor & ottimizzazioni.

--- XPathDataExtractor/src/main/java/it/uniroma3/agiw/XPathDataExtractor.java
+++ XPathDataExtractor/src/main/java/it/uniroma3/agiw/XPathDataExtractor.java
@@ -21,7 +21,7 @@ public class XPathDataExtractor implements XPathProgram {
 	private String outJson;
 	private XPathExecutor executor;
 	private String[] strFunctions;
-	
+
 	public XPathDataExtractor(String sourceJson, String inJson, String outJson, String[] strFunctions) {
 		this.sourceJson = sourceJson;
 		this.inJson = inJson;
@@ -32,90 +32,118 @@ public class XPathDataExtractor implements XPathProgram {
 
 	public void execute() {
 		JSONParser parser = new JSONParser();
-		
+
 		try {
 			JSONObject srcJsonObj = (JSONObject)parser.parse(new FileReader(sourceJson));
 			JSONObject inJsonObj = (JSONObject)parser.parse(new FileReader(inJson));
-			
+
 			JSONObject siteOutObj = new JSONObject();
-			
+
 			for (Object key1 : srcJsonObj.keySet()) { //itera per sito
 				String keyStr1 = (String)key1;
-				
+
 				JSONObject siteSrcObj = (JSONObject) srcJsonObj.get(keyStr1);
 				JSONObject siteInObj = (JSONObject) inJsonObj.get(keyStr1);
-				
+
 				JSONObject productOutObj = new JSONObject();
-				
+
 				for (Object key2 : siteSrcObj.keySet()) { //itera per prodotto
 					String keyStr2 = (String)key2;
 					String productKey = keyStr2.substring(keyStr2.indexOf("-")+1);
-					
+
 					JSONArray productSrcArr = (JSONArray) siteSrcObj.get(keyStr2);
 					JSONArray productInArr = (JSONArray) siteInObj.get(productKey);
-					
+
 					JSONObject idTypeJsonObj = new JSONObject();
-					
-					for (Object ruleObj : productInArr) { //itera per ogni regola XPath
+
+					for (Object ruleObj : productInArr) { //inizializza gli array per ogni regola XPath
 						JSONObject jsonRuleObj = (JSONObject)ruleObj;
-						String rule = (String)jsonRuleObj.get("rule");
 						String attr_name = (String)jsonRuleObj.get("attribute_name");
-						
-						System.out.println("================ GETTING "+attr_name+" INFOS ================");
-						
-						JSONArray jsonArr = this.executeSingleXPathRule(rule, productSrcArr);
-						
-						idTypeJsonObj.put(attr_name, jsonArr);
+						idTypeJsonObj.put(attr_name, new JSONArray());
 					}
+
+					for (Object urlObj : productSrcArr) {
+						String url = (String)urlObj;
+						Document doc;
+						try {
+							System.out.println("Fetching from "+url);
+							doc = Jsoup.connect(url).get();
+							String html = doc.html();
+
+							JSONObject resultObjs = executeProductXPathRules(html, productInArr);
+
+							for (Object resultObjKey : resultObjs.keySet()) {
+								String attrCode = (String)resultObjKey;
+								Object resultObj = resultObjs.get(resultObjKey);
+								
+								JSONArray attrArr = (JSONArray) idTypeJsonObj.get(attrCode);
+								
+								JSONObject objToPush = new JSONObject();
+								objToPush.put(url, resultObj);
+								
+								attrArr.add(objToPush);
+								
+								System.out.println(attrCode + ": " + objToPush);
+								
+							}
+							System.out.println();
+							
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
 					productOutObj.put(productKey, idTypeJsonObj);
 					System.out.println(idTypeJsonObj);
 				}
 				siteOutObj.put(keyStr1, productOutObj);
 				System.out.println(productOutObj);
 			}
-			
+
 			FileWriter file = new FileWriter(this.outJson);
 			file.write(siteOutObj.toJSONString());
 			file.flush();
 			file.close();
-			
+
 		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	private JSONArray executeSingleXPathRule(String rule, JSONArray urlArr) {
-		JSONArray outArr = new JSONArray();
-		for(Object urlObj : urlArr) {
-			JSONObject jsonObj = new JSONObject();
-			
-			String url = (String)urlObj;
-			//String result = "";
-			Object result = "";
-			Document doc;
-			try {
-				doc = Jsoup.connect(url).get();
-				String html = doc.html();
-				List<String> resultList = this.executor.executeXPath(html, rule, strFunctions);
-				if (resultList.size() == 1)
-					result = resultList.get(0);
-				else if (resultList.size() > 1)  {
-					JSONArray resultArr = new JSONArray();
-					for(String res : resultList)
-						resultArr.add(res);
-					result = resultArr;
-				}
-			} catch (IOException | XPathExpressionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			jsonObj.put(url.replace("\\", ""), result);
-			outArr.add(jsonObj);
-			System.out.println(jsonObj);
+
+	private JSONObject executeProductXPathRules(String html, JSONArray rules) {
+		JSONObject outObj = new JSONObject();
+
+		for (Object ruleObj : rules) {
+			JSONObject jsonRuleObj = (JSONObject)ruleObj;
+			String rule = (String)jsonRuleObj.get("rule");
+			String attr_name = (String)jsonRuleObj.get("attribute_name");
+
+			Object xpathResult = this.executeSingleXPathRule(html, rule);
+
+			outObj.put(attr_name, xpathResult);
 		}
-		return outArr;
+		return outObj;
+	}
+
+	private Object executeSingleXPathRule(String html, String rule) {
+		Object result = "";
+		List<String> resultList;
+		try {
+			resultList = this.executor.executeXPath(html, rule, this.strFunctions);
+			if (resultList.size() == 1)
+				result = resultList.get(0);
+			else if (resultList.size() > 1)  {
+				JSONArray resultArr = new JSONArray();
+				for(String res : resultList)
+					resultArr.add(res);
+				result = resultArr;
+			}
+		} catch (XPathExpressionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
